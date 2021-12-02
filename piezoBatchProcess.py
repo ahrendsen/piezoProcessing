7e0a78f168e013a7aa684aaa75603fba689264da
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2021-12-02 13:32:12 -0600

    Added in comments for everything

--- piezoBatchProcess.py
+++ piezoBatchProcess.py
@@ -5,40 +5,67 @@ Created on Thu Dec  2 10:33:14 2021
 @author: karla
 """
 
+# Used for getting lists of filenames and easy
+# path name construction.
 import os
-import datetime
+# Import the function from the piezoProcess.py file.
 from piezoProcess import processFile
+# Import pandas for easy joining of datasets and writing to
+# the excel file.
 import pandas as pd
 
+# This is a shorthand to get the "Home" directory for the currently
+# logged in user.
 homeDir = os.path.expanduser("~")
+# The location of the foler that you want to process.
 photosDir = os.path.join(homeDir, "Desktop", "gitSpace", "piezoprocessing",
                          "data")
 
+# Grab an iterator that contains the files in the folder.
 files = os.scandir(photosDir)
-os.chdir(photosDir)
-
-voltages = [0,10.05]
-spacing = 400
-resultsv1 = []
+# Convert the iterator into a list
 files = list(files)
 
+# The voltages at which the data was collected. It is
+# assumed that the images will be collected in this order.
+# So the first files will be collected at 0 volts, then
+# the second at 10.05, then the third at 0 volts again.
+voltages = [0, 10.05]
+# The approximate spacing of the fringes, used to set a
+# bounds of where the program will search for a minimum.
+spacing = 400
+
+
+# %%
+# This cell should be converted into a for each loop
+# over the voltages list... but I'm lazy.
+resultsv1 = []
+# This for loop takes every nth file from the
+# files list, where n is the number of voltages.
 for file in files[::len(voltages)]:
     res = processFile(file, spacing, photosDir, photosDir)
     resultsv1.append(res)
-
+# This converts it into a dataframe for easy joining later.
 df1 = pd.DataFrame(resultsv1, columns=['First Minimum Position V1',
                                        'Second Minimum Position V1'])
+# This adds in a new column where each value is the same
+# for each row of the column, and they are all equal to
+# voltages[0]
 df1.insert(0, 'Voltage 1(V)', voltages[0])
+
 
 resultsv2 = []
 for file in files[1::len(voltages)]:
-    res = processFile(file, spacing, photosDir, "outDir")
+    res = processFile(file, spacing, photosDir, photosDir)
     resultsv2.append(res)
-
 df2 = pd.DataFrame(resultsv2, columns=['First Minimum Position V2',
                                        'Second Minimum Postion V2'])
 df2.insert(0, 'Voltage 2(V)', voltages[1])
 
+# %%
+
+# Joins the two voltage blocks together.
 dfAll = df1.join(df2)
 
-dfAll.to_excel(os.path.join(photosDir,"excelTest.xlsx"))
+# Exports the dataframe as an excel file.
+dfAll.to_excel(os.path.join(photosDir, "excelTest.xlsx"))
--- piezoProcess.py
+++ piezoProcess.py
@@ -8,26 +8,32 @@ import skimage
 from skimage import measure
 import matplotlib.pyplot as plt
 import os
-import datetime
 
 
 homeDir = os.path.expanduser("~")
 photosDir = os.path.join(homeDir, "Desktop", "gitSpace", "piezoprocessing",
                          "data")
 
+# The input file is the image to be analyzed.
+# The spacing is the pixels between the maximum and the minimum in the image.
+# The input directory is the folder that the input files are located in.
+# The output directory is where you want any output data to be saved (not
+# curretnly used.)
+
+
 def processFile(inputFile, spacing, inputDirectory, outputDirectory):
-    image = skimage.io.imread(os.path.join(inputDirectory,inputFile))
-    
+    image = skimage.io.imread(os.path.join(inputDirectory, inputFile))
+
     line = measure.profile_line(image,
-                                [len(image)/2,0],[len(image)/2,len(image[0])],
+                                [len(image)/2, 0],
+                                [len(image)/2, len(image[0])],
                                 mode='constant',
-                                linewidth = 60, order=5)
+                                linewidth=60, order=5)
     fig, ax = plt.subplots()
-    
-    ax.scatter(range(len(line)),line[:,0])
+
+    ax.scatter(range(len(line)), line[:, 0])
     ax.set_title(inputFile)
     ax.set_xlabel("X Position (pixel)")
     ax.set_ylabel("Intensity")
-    
-    return [1,2]
 
+    return [1, 2]
