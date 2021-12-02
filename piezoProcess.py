16555fd0aedc2ae693e3bd823923ea1a1a0f48b4
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2021-12-02 10:49:01 -0600

    Added line width and titles to figures

--- piezoProcess.py
+++ piezoProcess.py
@@ -15,11 +15,21 @@ homeDir = os.path.expanduser("~")
 photosDir = os.path.join(homeDir, "Desktop", "gitSpace", "piezoprocessing",
                          "data")
 
+fileBaseName = "DSC_0068.JPG"
 file = os.path.join(photosDir,
-                    "test.jpg")
+                    fileBaseName)
 image = skimage.io.imread(file)
 skimage.io.imshow(image)
 
-line = measure.profile_line(image,[len(image)/2,0],[len(image)/2,len(image[0])], mode='constant', order=1)
+fringeWidth = 800
+
+line = measure.profile_line(image,
+                            [len(image)/2,0],[len(image)/2,len(image[0])],
+                            mode='constant',
+                            linewidth = 60, order=5)
 fig, ax = plt.subplots()
-ax.scatter(range(len(line)),line[:,1])
+
+ax.scatter(range(len(line)),line[:,0])
+ax.set_title(fileBaseName)
+ax.set_xlabel("X Position (pixel)")
+ax.set_ylabel("Intensity")
