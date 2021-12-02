898efdce8bb6153e0bee8538787a55d0e9428dec
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2021-12-02 10:49:17 -0600

    Added file for running batch processing

---
+++ piezoBatchProcess.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  2 10:33:14 2021
+
+@author: karla
+"""
+
+import os
+import datetime
+from piezoProcess import processFile
+
+homeDir = os.path.expanduser("~")
+photosDir = os.path.join(homeDir, "Desktop", "gitSpace", "piezoprocessing",
+                         "data")
+
+files = os.scandir(photosDir)
+
+spacing = 400
+results = []
+for file in files:
+    res = processFile(file, spacing)
+    results.append(res)
+
