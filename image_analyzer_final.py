67d1952561390505b4fdc5e0d27ab0d03b4e90f0
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2022-01-28 16:41:56 -0600

    SORRY NIKO! Coding is too much fun, I can't stop.
    I changed quite a few things to make the code easier to read, I think,
    and also implemented a search for the vertical location of the center of
    the profile, rather than just using the center of the image.

--- image_analyzer_final.py
+++ image_analyzer_final.py
@@ -13,13 +13,12 @@ import skimage
 from skimage import io
 from skimage import measure
 import matplotlib.pyplot as plt
+# For plot image inset
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import os
 import datetime
 import numpy as np
 import bottleneck as bn 
-
-
 
 
 def process_file(filename, fringe_width, input_folder, output_folder,
@@ -31,21 +30,54 @@ def process_file(filename, fringe_width, input_folder, output_folder,
     """linewidth = 160 because we want to capture 2/3 of the fringe pattern,
     the edges are more susceptible to noise.
     """
-    yCoord = len(image)/2 
-    profile_linewidth = 120 # This is the total width i.e. distance up plus 
+    imageHeight = len(image)
+    imageWidth = len(image[0])
+    
+    yMidpoint = imageHeight/2 
+    xMidpoint = imageWidth/2
+    
+    # Here we take the profile of the image with a VERTICAL line.
+    # This allows us to determine the y coordinate of the center
+    # of the Gaussian beam. Since this examines the whole image, it takes
+    # some time to run. In the future, if this is used with the batch
+    # provess program it should probably be put in a 
+    # different function and run on just the first image in a sequence to 
+    # retrieve the desired y position, then use that same value for all of
+    # the images.
+    line2 = measure.profile_line(image,
+                                [0, xMidpoint], # (x,y) of start of line
+                                [imageHeight, xMidpoint], # (x,y) of end
+                                linewidth = imageWidth,
+                                mode='constant', order=1)
+    
+    line2 = line2[:,0] # Note: this line just takes the red pixel data
+    
+    # Here we can visualize the vertical Gaussian profile of the beam.
+    # fig2, ax2 = plt.subplots()
+    # ax2.scatter(range(len(line2)), line2)
+    
+    y_max_intensity_location = np.argmax(line2)
+    
+    profile_linewidth = 500  # This is the total width i.e. distance up plus
                              # distance down. The larger this is, the more
                              # time it takes to process each image. Using
                              # nearly the entire image (3000), takes something
                              # like 26 seconds to process a single image,
                              # but it results in much smoother data. This is
                              # comapred to 5 seconds for a linewidth of 120.
+                             
+    profile_y = y_max_intensity_location
     line = measure.profile_line(image,
-                                [yCoord, 0],
-                                [yCoord, len(image[0])],
+                                [profile_y, 0],
+                                [profile_y, imageWidth],
                                 linewidth = profile_linewidth,
                                 mode='constant', order=5)
-    line = line[:,0]
-    line = bn.move_mean(line, window=100, min_count=1)
+    line = line[:,0] # Note: this line just takes the red pixel data
+    # Here, you can "smooth" the data by taking a moving average.
+    # the window parameter tells you how many elements you take in the 
+    # average. The min_count allows it to use a single point at the edges
+    # as the average.
+    line = bn.move_mean(line, window=1, min_count=1)
     fig, ax = plt.subplot_mosaic([['top', 'top'],
                                   ['bottom_left', 'bottom_right']],
                                  constrained_layout=True)
@@ -56,12 +88,12 @@ def process_file(filename, fringe_width, input_folder, output_folder,
     ax['bottom_right'].scatter(range(len(line)), line)
     ax['bottom_left'].set_title("First Minimum")
     
-    # Make an inset of the actual image
+    # Make an inset of the actual image on the upper plot
     axins = inset_axes(ax['top'], width=1.35, height=.9, loc='upper left')
     axins.tick_params(labelleft=False, labelbottom=False)
     axins.imshow(image)
-    axins.axhspan(yCoord - profile_linewidth/2,
-                  yCoord + profile_linewidth/2,
+    axins.axhspan(profile_y - profile_linewidth/2,
+                  profile_y + profile_linewidth/2,
                   color='yellow',
                   alpha=.5)
     print(max(line))
@@ -102,9 +134,9 @@ def process_file(filename, fringe_width, input_folder, output_folder,
 
     return([filename, min_1, min_2])
 
-# homeDir = os.path.expanduser("~")
-# photosDir = os.path.join(homeDir, "Desktop", "gitSpace", "piezoprocessing",
-#                          "data")
+homeDir = os.path.expanduser("~")
+photosDir = os.path.join(homeDir, "gitSpace", "piezoprocessing",
+                          "data")
 
-# file = os.path.join(photosDir, "DSC_0068.JPG")
-# process_file(file, 500, photosDir, photosDir)
+file = os.path.join(photosDir, "DSC_0068.JPG")
+process_file(file, 500, photosDir, photosDir)
